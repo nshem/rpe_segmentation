d747cca5f64200d67f1b9f847f1d949d43a8f111
nshem <nogashemer@gmail.com>
2024-10-26 20:06:29 +0300

    minify css, rename example data, bugfix

--- src/gui/headers.py
+++ src/gui/headers.py
@@ -4,7 +4,7 @@ colors = Link(rel="stylesheet", href="./css/pico.colors.min.css", type="text/css
 css_utils = Link(rel="stylesheet", href="./css/utils.css", type="text/css")
 htmx_ws = Script(src="./js/htmx.min.js")
 js_utils = Script(src="./js/utils.js")
-picolink = Link(rel="stylesheet", href="./css/pico.cyan.css", type="text/css")
+picolink = Link(rel="stylesheet", href="./css/pico.cyan.min.css", type="text/css")
 favicon = Link(rel="icon", type="image/svg", href="./favicon.ico")
 
 headers = (picolink, htmx_ws, js_utils, favicon, colors, css_utils)
