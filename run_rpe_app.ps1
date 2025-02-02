d8948f84af9ffb727d868be8fdcd30f8ae19a3b1
yarinudi <yarinudi@mail.tau.ac.il>
2025-02-02 10:24:27 +0200

    Run app script

---
+++ run_rpe_app.ps1
@@ -0,0 +1,8 @@
+# Folder (current working directory)
+# cd C:\Users\Yarin\University\Appendix\Tal\Collaborations\rpe_segmentation
+
+# Activate Venv:
+.\venv\Scripts\activate
+
+# Run application:
+python .\src\main.py
